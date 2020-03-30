b031bac68c49509c7cb713524ffced34c00d05f5
Rodrigo <rodrigozura@gmail.com>
2020-03-30 05:03:07 -0300

    Guardando imagenes en MONGODB

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const { randomNumber } = require('../helpers/libs');
 const fs = require('fs-extra');
+
+const { Image } = require('../models');
+
 const ctrl = {};
 
 //aca es donde manejamos la logica
@@ -10,20 +13,45 @@ ctrl.index = (req, res) => {
      
 };
 
-ctrl.create = async (req, res) => {
-     const imgUrl = randomNumber();
-     const imageTempPath = req.file.path;
-     const ext = path.extname(req.file.originalname).toLowerCase();
-     const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+ctrl.create =  (req, res) => {
 
-     if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+     const saveImage = async () => {
+        const imgUrl = randomNumber();
+        const images = await Image.find({ filename: imgUrl });
+        if (images.length > 0) {
 
-       await fs.rename(imageTempPath, targetPath);
+            saveImage();
 
-     }
+        }else{
 
-     res.send('works!')
-     
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+      
+            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+      
+                    await fs.rename(imageTempPath, targetPath);
+                      const newImg = new Image({
+                        title: req.body.title,
+                        description: req.body.description,
+                        filename: imgUrl + ext,
+                      });
+                    const imageSaved = await newImg.save();
+                    res.send('works!')
+
+            }else{
+    
+                    await fs.unlink(imageTempPath);
+                    res.status(500).json({error: 'Only Images are allowed'});
+    
+            }
+
+        }
+        
+     };
+
+     saveImage();
+
 };
 
 ctrl.like = (req, res) => {
